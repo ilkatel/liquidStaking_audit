a2571f3ffd3fd7a18b815bde84a3c9cfac1e1400
DippyArtu <artur.makhnach@gmail.com>
2022-05-12 15:15:33 +0300

    compilation typo fix

--- packages/hardhat/contracts/nDistributor.sol
+++ packages/hardhat/contracts/nDistributor.sol
@@ -24,7 +24,7 @@ import "./interfaces/DNTInterface.sol";
  * Features:
  * - Ownable
  */
-contract nDistributor is Ownable {
+contract NDistributor is Ownable {
 
     // ------------------------------- USER MANAGMENT
 
