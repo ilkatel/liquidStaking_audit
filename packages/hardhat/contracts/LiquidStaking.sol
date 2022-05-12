2e39f1599b51bd0d91f3dd8b08824b130a59a5d5
Daniil Rashin <d.rashin@clicknet.pro>
2022-05-12 15:23:42 +0300

    Fix compile errors & natspec

--- packages/hardhat/contracts/LiquidStaking.sol
+++ packages/hardhat/contracts/LiquidStaking.sol
@@ -8,10 +8,9 @@
 
 pragma solidity ^0.8.4;
 
-import "./Interfaces/nASTRInterface.sol";
 import "./nDistributor.sol";
-import "../libs/openzeppelin/contracts/utils/Counters.sol";
-import "../libs/openzeppelin/contracts/access/Ownable.sol";
+import "../libs/@openzeppelin/contracts/utils/Counters.sol";
+import "../libs/@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title								Liquid staking contract
@@ -22,66 +21,66 @@ contract LiquidStaking is Ownable {
 	using Counters for Counters.Counter;
 
     // -------------------------------- DISTRIBUTOR
-	/// @notice							distributor address etc.
+	// @notice							distributor address etc.
 	address public						distrAddr;
 	nDistributor						distr;
 
 
     // -------------------------------- STAKING SETTINGS
-	/// @notice							minimum staking amount
+	// @notice							minimum staking amount
 	uint256 public						minStake;
-	/// @notice							stores staking timeframes, not sure if it's the right way
+	// @notice							stores staking timeframes, not sure if it's the right way
 	uint256[] public					stakingTerms;
 
 	
     // -------------------------------- STAKING MANAGEMENT
 	Counters.Counter					stakeIDs;
-	/// @notice							single stake data
+	// @notice							single stake data
 	struct								Stake {
 		uint256							balance;
 		uint256							startDate;
 		uint256							finDate;
 	}
-	/// @notice							store stakes by IDs
+	// @notice							store stakes by IDs
 	mapping(uint256 => Stake) public	stakes;
 
 
     // -------------------------------- EVENTS
-	/// @notice events on calling stake/reedem, indexed by sender
+	// @notice events on calling stake/reedem, indexed by sender
 	event Staked(address indexed from, uint256 amount, uint256 timeframe);
 	event Reedemed(address indexed to, uint256 amount);
 
 
     // -------------------------------------------------------------------------------------------------------
     // ------------------------------- FUNCTIONS
-	/// @param							[address] _dAddr => distributor address
-	/// @param							[uint256] _min => minimum ASTR amount to stake
+	// @param							[address] _dAddr => distributor address
+	// @param							[uint256] _min => minimum ASTR amount to stake
 	constructor(address _dAddr, uint256 _min){
-		distAddr = _dAddr;
-		distr = nDistributor(distAddr);
+		distrAddr = _dAddr;
+		distr = nDistributor(distrAddr);
 		minStake = _min;
 	}
 
-	/// @notice							add new staking timeframe
-	/// @param							[uint256] newT => new staking timeframe to add
+	// @notice							add new staking timeframe
+	// @param							[uint256] newT => new staking timeframe to add
 	function							addTerm(uint256 newT) external onlyOwner {
 		stakingTerms.push(newT);
 	}
 
-	/// @notice							change staking timeframe by index
-	/// @param							[uint8] termN => index of term to change
-	/// @param							[uint256] newT => new staking timeframe to add
+	// @notice							change staking timeframe by index
+	// @param							[uint8] termN => index of term to change
+	// @param							[uint256] newT => new staking timeframe to add
 	function							changeTerm(uint8 termN, uint256 newT) external onlyOwner {
 		stakingTerms[termN] = newT;
 	}
 
-	/// @notice							receive native tokens, save Stake data, give nASTR
-	/// @param							[uint8] termN => index of desired staking duration
-	/// @return							[uint256] id => ID of created stake
+	// @notice							receive native tokens, save Stake data, give nASTR
+	// @param							[uint8] termN => index of desired staking duration
+	// @return							[uint256] id => ID of created stake
 	function 							stake(uint8 termN) external payable returns (uint256 id) {
 
 		// check if LS utility is active & we got enough value
-		bool							uActive = distr.utilities[utilityID["LS"]].active;
+		bool							uActive = distr.utilities[distr.utilityID["LS"]].active;
 		require(uActive, "Inactive utility!");
 		require(msg.value >= minStake, "Value less than minimum stake amount");
 
@@ -99,13 +98,13 @@ contract LiquidStaking is Ownable {
 		emit Staked(msg.sender, msg.value, stakingTerms[termN]);
 	}
 
-	/// @notice							check if Stake is redeemable, burn nASTR, send ASTR
-	/// @param							[uint256] id => Stake id
-	/// @param							[uint256] amount => reedem amount
+	// @notice							check if Stake is redeemable, burn nASTR, send ASTR
+	// @param							[uint256] id => Stake id
+	// @param							[uint256] amount => reedem amount
 	function 							reedem(uint256 id, uint256 amount) external {
 
 		// should we check if utility is active?
-		bool							active = distr.utilities[utilityID["LS"]].active;
+		bool							active = distr.utilities[distr.utilityID["LS"]].active;
 		require(active, "Inactive utility!");
 
 		require(amount > 0, "Invalid amount!");
