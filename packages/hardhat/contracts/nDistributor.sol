9c06d9c34169ed41b069179055f5ab113ffe007d
DippyArtu <artur.makhnach@gmail.com>
2022-05-20 01:02:12 +0300

    nDistributor DNT burn logic

--- packages/hardhat/contracts/nDistributor.sol
+++ packages/hardhat/contracts/nDistributor.sol
@@ -7,9 +7,10 @@
 // - Get user utils [+]
 // - Get user DNT in util [+]
 // - Get user liquid DNT [+]
+// - Get user utils in dnt [+]
 //
 // - Add DNT balance getter function for user from DNT contract [+]
-// - DNT removal (burn) logic
+// - DNT removal (burn) logic [+]
 // - Token transfer logic (should keep track of user utils)
 //
 // - Make universal DNT interface
@@ -190,12 +191,6 @@ contract NDistributor is Ownable {
         return (users[_user].userDnts);
     }
 
-    // @notice                         returns a list of user's utilities
-    // @param                          [address] _user => user address
-    function                           listUserUtilities(address _user) public view returns(string[] memory) {
-        return (users[_user].userUtilities);
-    }
-
     // @notice                         returns ammount of liquid DNT toknes in user's possesion
     // @param                          [address] _user => user address
     // @param                          [string] _dnt => DNT token name
@@ -207,8 +202,15 @@ contract NDistributor is Ownable {
     // @param                          [address] _user => user address
     // @param                          [string] _util => utility name
     // @param                          [string] _dnt => DNT token name
-    function                           getUserDntInUtil(address _user, string memory _util, string memory _dnt) public view returns(uint256) {
+    function                           getUserDntBalanceInUtil(address _user, string memory _util, string memory _dnt) public view returns(uint256) {
         return (users[_user].dnt[_dnt].dntInUtil[_util]);
+    }
+
+    // @notice                         returns which utilities are used with specific DNT token
+    // @param                          [address] _user => user address
+    // @param                          [string] _dnt => DNT token name
+    function                           getUserUtilsInDnt(address _user, string memory _dnt) public view returns(string[] memory) { // <--------- doesn;t return
+        return (users[_user].dnt[_dnt].userUtils);
     }
 
     // @notice                         returns user's DNT balance
@@ -235,17 +237,17 @@ contract NDistributor is Ownable {
     // @param                          [string] _dnt => minted dnt
     function                           issueDNT(address _to, uint256 _amount, string memory _utility, string memory _dnt) public onlyOwner {
         uint256                        id;
-        address                        user = msg.sender;
 
         require(DNTContractAdress != address(0x00), "Interface not set!");
         require((id = utilityId[_utility]) > 0, "Non-existing utility!");
         require(utilityDB[id].isActive == true, "Inactive utility!");
 
-        _addDntToUser(_dnt, users[user].userDnts);
-        _addUtilityToUser(_utility, users[user].userUtilities);
+        _addDntToUser(_dnt, users[_to].userDnts);
+        _addUtilityToUser(_utility, users[_to].userUtilities);
+        _addUtilityToUser(_utility, users[_to].dnt[_dnt].userUtils);
 
-        users[user].dnt[_dnt].dntInUtil[_utility] += _amount;
-        users[user].dnt[_dnt].dntLiquid += _amount;
+        users[_to].dnt[_dnt].dntInUtil[_utility] += _amount;
+        users[_to].dnt[_dnt].dntLiquid += _amount;
         DNTContract.mintNote(_to, _amount);
     }
 
@@ -292,21 +294,63 @@ contract NDistributor is Ownable {
     // @param                          [uint256] _amount => amount of tokens to burn
     // @param                          [string] _utility => minted dnt utility
     // @param                          [string] _dnt => minted dnt
-    function                           removeDNT(address _account, uint256 _amount, string memory _utility, string memory _dnt) public onlyOwner { // <--------------------- WIP ------------ manage DNT removal
+    function                           removeDNT(address _account, uint256 _amount, string memory _utility, string memory _dnt) public onlyOwner {
         uint256                        id;
-        address                        user = msg.sender;
 
         require(DNTContractAdress != address(0x00), "Interface not set!");
 
         require((id = utilityId[_utility]) > 0, "Non-existing utility!");
         require(utilityDB[id].isActive == true, "Inactive utility!");
 
-        require((users[user].dnt[_dnt].dntInUtil[_utility] - _amount) > 0, "Not enough DNT in utility!");
-        require((users[user].dnt[_dnt].dntLiquid - _amount) > 0, "Not enough liquid DNT!");
+        require(users[_account].dnt[_dnt].dntInUtil[_utility] >= _amount, "Not enough DNT in utility!");
+        require(users[_account].dnt[_dnt].dntLiquid >= _amount, "Not enough liquid DNT!");
 
-        users[user].dnt[_dnt].dntInUtil[_utility] += _amount;
-        users[user].dnt[_dnt].dntLiquid += _amount;
+        users[_account].dnt[_dnt].dntInUtil[_utility] -= _amount;
+        users[_account].dnt[_dnt].dntLiquid -= _amount;
+
+        if (users[_account].dnt[_dnt].dntInUtil[_utility] == 0) {
+            _removeUtilityFromUser(_utility, users[_account].userUtilities);
+            _removeUtilityFromUser(_utility, users[_account].dnt[_dnt].userUtils);
+        }
+        if (users[_account].dnt[_dnt].dntLiquid == 0) {
+            _removeDntFromUser(_dnt, users[_account].userDnts);
+        }
+
         DNTContract.burnNote(_account, _amount);
+    }
+
+    // @notice                         removes utility string from user array of utilities
+    // @param                          [string] _utility => name of the utility token
+    // @param                          [string[] ] localUserUtilities => array of user's utilities
+    function                           _removeUtilityFromUser(string memory _utility, string[] storage localUserUtilities) internal onlyOwner {
+        uint                           l;
+        uint                           i = 0;
+
+        l = localUserUtilities.length;
+        for (i; i < l; i++) {
+            if (keccak256(abi.encodePacked(localUserUtilities[i])) == keccak256(abi.encodePacked(_utility))) {
+                delete localUserUtilities[i];
+                return;
+            }
+        }
+        return;
+    }
+
+    // @notice                         removes DNT string from user array of DNTs
+    // @param                          [string] _dnt => name of the DNT token
+    // @param                          [string[] ] localUserDnts => array of user's DNTs
+    function                           _removeDntFromUser(string memory _dnt, string[] storage localUserDnts) internal onlyOwner {
+        uint                           l;
+        uint                           i = 0;
+
+        l = localUserDnts.length;
+        for (i; i < l; i++) {
+            if (keccak256(abi.encodePacked(localUserDnts[i])) == keccak256(abi.encodePacked(_dnt))) {
+                delete localUserDnts[i];
+                return;
+            }
+        }
+        return;
     }
 
     // transfer tokens (should keep track of util)
