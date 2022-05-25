c792f0b11ab49ec3e8c8405e1b03f70d8a0f8ffb
DippyArtu <artur.makhnach@gmail.com>
2022-05-26 01:43:42 +0300

    nASTR args

--- packages/hardhat/contract-arguments/nASTR-args.js
+++ packages/hardhat/contract-arguments/nASTR-args.js
@@ -1,10 +1,5 @@
 // nASTR Contract arguments
 
-// module.exports = [
-//   "0x95e9450e2737e2239Be1CE225D79E4B2bE171f71", // DNT distributor contract address (will become the owner)
-// ];
-
-// Hardhat dev
 module.exports = [
   "0x5D5ed321b17EAcDD4D0EaCb957da890b6B6761c0", // DNT distributor contract address (will become the owner)
 ];
