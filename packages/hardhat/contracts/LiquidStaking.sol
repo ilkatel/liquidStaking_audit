83d95b6f7e2088ffedef1d397993680ad4936288
Daniil Rashin <d.rashin@clicknet.pro>
2022-06-01 17:31:42 +0300

    add shibuya-tested Liquid Staking, DAPPS_STAKING features not yet done

--- packages/hardhat/contracts/LiquidStaking.sol
+++ packages/hardhat/contracts/LiquidStaking.sol
@@ -1,222 +1,199 @@
 //SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.4;
 
+import "../libs/@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "../libs/@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "./interfaces/DappsStaking.sol";
 import "./nDistributor.sol";
-import "./Staker.sol";
-import "../libs/@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "../libs/@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "../libs/@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "../libs/@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
-
-/**
- * @title Liquid staking contract
- */
+/*
+inside one era:
+staker/dapp rewards claimed
+pools filled
+stakes/unstakes accepted
+bond/unbond called
+*/
 contract LiquidStaking is Initializable, AccessControlUpgradeable {
-    using CountersUpgradeable for CountersUpgradeable.Counter;
     DappsStaking public constant DAPPS_STAKING = DappsStaking(0x0000000000000000000000000000000000005001);    
+    bytes32 public constant            MANAGER = keccak256("MANAGER");
 
+    string public utilName;
+    string public DNTname;
+    uint256 public minStake;
+    uint256 public withdrawBlock;
 
-    // DECLARATIONS
-    //
-    // -------------------------------------------------------------------------------------------------------
-    // ------------------------------- STAKING SETTINGS
-    // -------------------------------------------------------------------------------------------------------
+    uint256 public unstakingPool;
+    uint256 public rewardPool;
 
-    // @notice        core staking settings
-    uint256   public    totalBalance;
-    uint256   public    unstakingPool;
-    uint256   public    minStake;
-
-    // @notice DNT distributor
     address public distrAddr;
     NDistributor   distr;
 
-    // @notice    nDistributor required values
-    string public utilName; // Liquid Staking utility name
-    string public DNTname; // DNT name
-
-    // @notice Stake struct
-    struct      Stake {
-        uint256 eraStaked;
+    struct Stake {
+        uint256 totalBalance;
+        uint256 eraStarted;
     }
-
-    // @notice Stakes mapping to user
     mapping(address => Stake) public stakes;
 
-    // @notice Withdrawal struct
     struct Withdrawal {
         uint256 val;
         uint256 eraReq;
     }
-    // @notice Withdrawals array mapping to user
     mapping(address => Withdrawal[]) public withdrawals;
 
-    // @notice eras until withdraw possible
-    uint256   public    withdrawEras;
-    address public stakerAddr;
-    Staker staker;
-    uint256 public rewardsPool;
+    struct eraData {
+        bool done;
+        uint256 val;
+    }
+    mapping(uint256 => eraData) public eraStaked; // total tokens staked per era
+    mapping(uint256 => eraData) public eraUnstaked; // total tokens unstaked per era
+    mapping(uint256 => eraData) public eraStakerReward; // total staker rewards per era
+    mapping(uint256 => eraData) public eraDappReward; // total dapp rewards per era
+    mapping(uint256 => eraData) public eraRevenue; // total revenue per era
 
-    bytes32 public constant            STAKER = keccak256("STAKER");
-    bytes32 public constant            ADMIN = keccak256("ADMIN");
-
-    mapping(uint256 => uint256) public eraStaked;
-    mapping(uint256 => uint256) public eraUnstaked;
-    // FUNCTIONS
-    //
-    // -------------------------------------------------------------------------------------------------------
-    // ------------------------------- INITIALIZER 
-    // -------------------------------------------------------------------------------------------------------
-    // @notice set distributor and DNT addresses, minimum staking amount
-    // @param  [address] _distrAddr => DNT distributor address
-    function initialize(address _distrAddr) public initializer {
-
-        // @dev set distributor address and contract instance
-        distrAddr = _distrAddr;
-        distr = NDistributor(distrAddr);
-
-        utilName = "LiquidStaking"; // Liquid Staking utility name
-        DNTname  = "nASTR"; // DNT name
-
-        stakerAddr = "/* put deployed staker contract address here */";
-        staker = Staker(stakerAddr);
-
-        minStake = 5 ether;
-        withdrawEras = 10;
+    function initialize() public initializer {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(ADMIN, msg.sender);
-        _grantRole(STAKER, stakerAddr);
+        _grantRole(MANAGER, msg.sender);
     }
 
-    function fillRewardsPool() external payable onlyRole(STAKER) { // rewards goes here
-        require(msg.value > 0, "0 value!");
-        rewardsPool += msg.value;
-    }
-
-    function fillUnstakingPool() external payable onlyRole(STAKER) { // 10% revenue from staker goes here
-        require(msg.value > 0, "0 value!");
-        unstakingPool += msg.value;
+    function setup() external onlyRole(MANAGER) {
+        withdrawBlock = DAPPS_STAKING.read_unbonding_period();
+        DNTname = "nASTR";
+        utilName = "LiquidStaking";
+        //DAPPS_STAKING.set_reward_destination(DappsStaking.RewardDestination.FreeBalance);
     }
 
 
-    // -------------------------------------------------------------------------------------------------------
-    // ------------------------------- ADMIN
-    // -------------------------------------------------------------------------------------------------------
-
-    // @notice set distributor
-    // @param  [address] a => new distributor address
-    function   setDistr(address a) external onlyRole(ADMIN) {
-        distrAddr = a;
+    // ADMIN FUNCS
+    function set_distr(address _newDistr) public onlyRole(MANAGER) {
+        distrAddr = _newDistr;
         distr = NDistributor(distrAddr);
     }
 
-    // @notice set minimum stake value
-    // @param  [uint256] v => new minimum stake value
-    function   setMinStake(uint256 v) external onlyRole(ADMIN) {
-        minStake = v;
+    function set_min(uint256 _val) public onlyRole(MANAGER) {
+        minStake = _val;
     }
 
-
-    // -------------------------------------------------------------------------------------------------------
-    // ------------------------------- VIEWS
-    // -------------------------------------------------------------------------------------------------------
-
-    // @notice returns current era number from DAPPS_STAKING module
-    function   current_era() public view returns(uint256) {
+    function current_era() public view returns(uint256) {
         return DAPPS_STAKING.read_current_era();
     }
 
-    // @notice returns apr in %
-    function   get_apr() public view returns(uint256) {
-        uint32 era = uint32(DAPPS_STAKING.read_current_era() - 1);
-        return (DAPPS_STAKING.read_era_staked(era) / DAPPS_STAKING.read_era_reward(era) / 100); // divide total staked by total rewards for prev era
+
+    // GLOBAL FUNCS
+    // @dev stake for given era
+    function global_stake(uint256 _era) external {
+        eraData storage d = eraStaked[_era];
+
+        require(!d.done, "Already done!");
+
+        d.done = true;
+
+        DAPPS_STAKING.bond_and_stake(address(this), uint128(d.val));
     }
 
-    // @notice returns user active withdrawals
-    function get_user_withdrawals() public view returns(Withdrawal[] memory) {
-        return withdrawals[msg.sender];
+    // @dev unstake for given era
+    function global_unstake(uint256 _era) external {
+        eraData storage d = eraUnstaked[_era];
+
+        require(!d.done, "Already done!");
+
+        d.done = true;
+
+        DAPPS_STAKING.unbond_and_unstake(address(this), uint128(d.val));
     }
 
-    function calculate_reward() public view returns (uint256) {
-        uint256 uBalance = distr.getUserDntBalanceInUtil(msg.sender, utilName, DNTname);
-        uint256 part = totalBalance / uBalance / 100; // % of total
-        return rewardsPool / 100 * part; // so user can get equal part of rewards pool
-
+    // @dev withdraw
+    function global_withdraw() external {
+        uint256 p = address(this).balance;
+        DAPPS_STAKING.withdraw_unbonded();
+        uint256 a = address(this).balance;
+        unstakingPool += a - p;
     }
 
-    // -------------------------------------------------------------------------------------------------------
-    // ------------------------------- STAKE
-    // -------------------------------------------------------------------------------------------------------
+    function claim_dapp(uint256 _era) external {
+        require(current_era() != _era, "Cannot claim yet!");
+        require(eraDappReward[_era].val == 0, "Already claimed!");
+        uint256 p = address(this).balance;
+        DAPPS_STAKING.claim_dapp(address(this), uint128(_era));
+        uint256 a = address(this).balance;
+        eraDappReward[_era].val = a - p;
+        eraRevenue[_era].val += eraDappReward[_era].val / 100 * 10; // 10% goes to revenue pool
+    }
 
-    // @notice create new stake with desired timeframe
-    function   stake() external payable {
-        uint256 val = msg.value;
-		require(val >= minStake, "Value less than minimum stake amount");
+    function claim_user(uint256 _era) external {
+        uint256 p = address(this).balance;
+        DAPPS_STAKING.claim_staker(address(this));
+        uint256 a = address(this).balance;
+        eraStakerReward[_era].val = a - p;
+        eraRevenue[_era].val += eraStakerReward[_era].val / 100; // 1% goes to revenue pool
+    }
 
+    function fill_pools(uint256 _era) external {
+        require(!eraRevenue[_era].done, "Already done!");
+        require(!eraStakerReward[_era].done, "Already done!");
+        eraRevenue[_era].done = true;
+        eraStakerReward[_era].done = true;
+        unstakingPool += eraRevenue[_era].val / 100 * 10; // 10% of revenue goes to unstaking pool
+        rewardPool += eraStakerReward[_era].val;
+    }
+
+
+    // USER FUNCS
+    function stake() external payable {
         Stake storage s = stakes[msg.sender];
         uint256 era = current_era();
 
-        s.eraStaked = s.eraStaked == 0 ? era : s.eraStaked;
+        require(msg.value > minStake, "Send at least min stake value!");
 
-        totalBalance += val;
-        eraStaked[era] += val;
+        eraStaked[era].val += msg.value;
 
-        distr.issueDnt(msg.sender, val, utilName, DNTname);
-
-        // send funds to staker
-        payable(stakerAddr).call{value: msg.value};
+        s.totalBalance += msg.value;
+        s.eraStarted = s.eraStarted == 0 ? era : s.eraStarted;
+        distr.issueDnt(msg.sender, msg.value, utilName, DNTname);
     }
 
-    // @notice unstake DNTs to retrieve native tokens from stake
-    // @param  [uint256] amount => amount of tokens to unstake
-    // @param  [bool] immediate => false if call unbond, 'true' not yet implemented
-    function   unstake(uint256 amount, bool immediate) external {
-        require(amount > 0, "Invalid amount!");
+    function unstake(uint256 _amount, bool _immediate) external {
+        Stake storage s = stakes[msg.sender];
+        uint256 era = current_era();
+        require(_amount > 0, "Invalid amount!");
+        require(s.totalBalance >= _amount, "Invalid amount!");
 
-        uint256 uBalance = distr.getUserDntBalanceInUtil(msg.sender, utilName, DNTname);
-        require(uBalance >= amount, "Insuffisient DNT balance!");
-        totalBalance -= amount;
-        eraUnstaked[current_era()] += amount;
+        eraUnstaked[era].val += _amount;
 
-        if (immediate) {
-            unstakingPool -= amount;
-            uint256 coms = amount / 100; // 1% immedieate coms
-            payable(msg.sender).call{value: amount - coms};
-        }else {
+        s.totalBalance -= _amount;
+        if(s.totalBalance == 0) {
+            s.eraStarted = 0;
+        }
+
+        if (_immediate) {
+            uint256 fee = _amount / 100; // 1% immediate unstaking fee
+            eraRevenue[era].val += fee;
+            distr.removeDnt(msg.sender, _amount, utilName, DNTname);
+            payable(msg.sender).call{value: _amount - fee};
+        } else {
             withdrawals[msg.sender].push(Withdrawal({
-                val: amount,
-                eraReq: current_era()
+                val: _amount,
+                eraReq: era
             }));
         }
     }
 
-
-    // -------------------------------------------------------------------------------------------------------
-    // ------------------------------- CLAIM
-    // -------------------------------------------------------------------------------------------------------
-
-    // @notice claim user rewards
-    function claim_user() external {
-        uint256 reward = calculate_reward();
-        require(rewardsPool >= reward, "Rewards pool has less liquidity!");
-        rewardsPool -= reward;
-        payable(msg.sender).call{value: reward};
-
+    function claim() external {
+        /* 
+        send reward to user from rewards pool according to their share
+        */
     }
-    
 
-    // -------------------------------------------------------------------------------------------------------
-    // ------------------------------- WITHDRAW
-    // -------------------------------------------------------------------------------------------------------
-    
-    // @notice withdraw unbonded
-    function withdraw(uint id) external {
-        Withdrawal storage w = withdrawals[msg.sender][id]; 
-        require(current_era() - w.eraReq > withdrawEras, "Not enough eras passed!");
-        unstakingPool -= w.val;
+    function withdraw(uint256 _id) external {
+        Withdrawal storage w = withdrawals[msg.sender][_id];
+        uint256 val = w.val;
+
+        require(current_era() - w.eraReq >= withdrawBlock, "Not enough eras passed!");
+        require(unstakingPool >= val, "Unstaking pool drained!");
+
         w.eraReq = 0;
-        distr.removeDnt(msg.sender, w.val, utilName, DNTname);
-        payable(msg.sender).call{value: w.val};
+
+        distr.removeDnt(msg.sender, val, utilName, DNTname);
+        payable(msg.sender).call{value: val};
     }
+
 }
