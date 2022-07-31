02f6bdd398c5a0e810153aabd2c8422fdb378d81
Aleksey <44026295+azhlbn@users.noreply.github.com>
2022-07-31 21:07:29 +0300

    Update SiriusHandler.sol
    Added zero address check

--- packages/hardhat/contracts/SiriusHandler.sol
+++ packages/hardhat/contracts/SiriusHandler.sol
@@ -6,6 +6,7 @@ interface IPool {
     function getVirtualPrice() external view returns (uint256);
     function getTokenBalance(uint8 index) external view returns (uint256);
     function getTokenIndex(address tokenAddress) external view returns (uint8 tokenIndex);
+    function getToken(uint8 index) external view returns (address);
 }
 
 interface IToken {
@@ -44,15 +45,20 @@ contract SiriusHandler {
     }
 
     // @notice calculates nTokens share for user in pool
-    function calc(address _user) external view returns (uint sum) {
+    function calc(address _user) external view returns (uint) {
         require(msg.sender == liquid || msg.sender == owner, "Only for Algem or owner");
+        require(_user != address(0), "Zero address error");
+        uint sum;
         uint userLpBal = lp.balanceOf(_user);
         uint userGaugeBal = gauge.balanceOf(_user);
         uint virtualPrice = pool.getVirtualPrice() / 10**18;
         uint nTokensInPool = pool.getTokenBalance(idxNtoken);
         uint tokensInPool = pool.getTokenBalance(idxToken);
-
+        if (nTokensInPool == 0 || tokensInPool == 0) {
+            return 0;
+        }
         sum = ((userLpBal + userGaugeBal) * virtualPrice) * nTokensInPool / (tokensInPool + nTokensInPool);
+        return sum;
     }
 
 }
