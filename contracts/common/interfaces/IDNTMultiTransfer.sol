2152dc7ab5e74a7915c7a8e4b1ab776abdf96423
ilkatel <ilkatel@inbox.ru>
2022-10-14 15:58:40 +0300

    remove some files

--- contracts/common/interfaces/IDNTMultiTransfer.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-// @notice DNT token contract interface
-interface IDNTMultiTransfer {
-  function transferFromUtilities(address to, uint256[] memory amounts, string[] memory utilities) external;
-}
--- contracts/upgredeable/interfaces/IPartnerHandler.sol
+++
@@ -1,6 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-interface IPartnerHandler {
-    function calc(address) external view returns (uint);
-}
